0df3d7cf1c171200850f2955c02793bdf29d1d7f
loukaspe20 <loukaspe20@gmail.com>
2021-03-31 08:49:19 +0300

    Adds program arguments handling

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
-#include <iostream>
+#include "Helper/Helper.h"
+#include <getopt.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+const char* PROGRAM_OPTIONS = "c:b:";
+const char* WRONG_PROGRAM_USAGE_ERROR = "Usage %s -c [citizenRecordsFile] -b [bloom size]\n";
+
+int main(int argc, char* argv[])
+{
+    int opt;
+
+    char* citizenRecordsFile;
+    int bloomSizeInKiloBytes;
+
+    while ( (opt = getopt(argc, argv, PROGRAM_OPTIONS)) != -1 ) {
+        switch (opt) {
+            case 'c':
+                citizenRecordsFile = Helper::copyString(optarg);
+                break;
+            case 'b':
+                bloomSizeInKiloBytes = atoi(optarg);
+                break;
+            default:
+                Helper::handleError(WRONG_PROGRAM_USAGE_ERROR);
+        }
+    }
+
     return 0;
 }
