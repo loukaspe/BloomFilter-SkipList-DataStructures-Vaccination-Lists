13e1c4ee82bffa60e990ed5025081b72a2499c55
loukaspe20 <loukaspe20@gmail.com>
2021-04-07 20:53:33 +0300

    Adds main cpp skeleton

--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
-#include "Helper/Helper.h"
+#include "CitizenRecordsFileReader/CitizenRecordsFileReader.h"
+#include "MenuPromptCreator/MenuPromptCreator.h"
 #include <getopt.h>
 
-const char* PROGRAM_OPTIONS = "c:b:";
-const char* WRONG_PROGRAM_USAGE_ERROR = "Usage %s -c [citizenRecordsFile] -b [bloom size]\n";
+const char *PROGRAM_OPTIONS = "c:b:";
+const char *WRONG_PROGRAM_USAGE_ERROR = "Usage %s -c [citizenRecordsFile] -b [bloom size]\n";
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char *argv[]) {
     int opt;
 
-    char* citizenRecordsFile;
+    char *citizenRecordsFile;
     int bloomSizeInKiloBytes;
 
-    while ( (opt = getopt(argc, argv, PROGRAM_OPTIONS)) != -1 ) {
+    while ((opt = getopt(argc, argv, PROGRAM_OPTIONS)) != -1) {
         switch (opt) {
             case 'c':
                 citizenRecordsFile = Helper::copyString(optarg);
@@ -23,6 +23,28 @@ int main(int argc, char* argv[])
                 Helper::handleError(WRONG_PROGRAM_USAGE_ERROR);
         }
     }
+
+    PersonLinkedList *people = new PersonLinkedList();
+    VirusLinkedList *viruses = new VirusLinkedList();
+
+    CitizenRecordsFileReader *fileReader = new CitizenRecordsFileReader(
+            citizenRecordsFile,
+            people,
+            viruses
+    );
+
+    fileReader->readAndUpdateStructures();
+
+    VaccinationCenter *vaccinationCenter = new VaccinationCenter(
+            people,
+            viruses
+    );
+
+    MenuPromptCreator *menuPromptCreator = new MenuPromptCreator(
+            vaccinationCenter
+    );
+
+    menuPromptCreator->createAndExecute();
 
     return 0;
 }
