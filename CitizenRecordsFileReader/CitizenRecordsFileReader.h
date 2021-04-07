b708e712e04f1a4322257d6ecdd9cb59513ff6e7
loukaspe20 <loukaspe20@gmail.com>
2021-04-07 19:50:12 +0300

    Adds input file reader

---
+++ CitizenRecordsFileReader/CitizenRecordsFileReader.h
@@ -0,0 +1,24 @@
+#ifndef ERGASIA1_CITIZENRECORDSFILEREADER_H
+#define ERGASIA1_CITIZENRECORDSFILEREADER_H
+
+
+#include "../PersonLinkedList/PersonLinkedList.h"
+#include "../VirusLinkedList/VirusLinkedList.h"
+
+class CitizenRecordsFileReader {
+public:
+    CitizenRecordsFileReader(char *fileName, PersonLinkedList *people, VirusLinkedList *viruses);
+    void readAndUpdateStructures();
+private:
+    char *fileName;
+    PersonLinkedList *people;
+    VirusLinkedList *viruses;
+    static const char *NO_INPUT_FILE_ERROR;
+    static const char *OPEN_FILE_ERROR;
+    static const char *OPEN_FILE_READ_MODE;
+    static const char *SPACE_DELIMITER;
+    static const char *ERROR_IN_READING_FILE;
+    bool setBooleanIsVaccinatedFromStringValue(char*);
+};
+
+#endif //ERGASIA1_CITIZENRECORDSFILEREADER_H
